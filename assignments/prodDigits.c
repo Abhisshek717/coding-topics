685a49e398cd4a5fef4fcff960ddbc5b4ca639b0
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2023-04-17 15:19:51 +0530

    product of a number
    topic: function

---
+++ assignments/prodDigits.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("%d\n",prodDigits(i));
+    return 0;
+}
+int prodDigits(int n){
+    return n*n;
+}
