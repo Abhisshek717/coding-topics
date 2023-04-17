ff855900a96c69b8d271840b8ebb38ffda4b720f
Abhishek <46296076+Abhisshek717@users.noreply.github.com>
2023-04-17 15:11:46 +0530

    added amstrong number
    topic: functions

---
+++ assignments/amstrong__number.c
@@ -0,0 +1,32 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printAmstrong(123);
+    isAmstrong(123);
+    return 0;
+}
+int cubesum(int n){
+    int sum = 0;
+    int rem;
+    while(n>0){
+        rem = n % 10;
+        sum = sum + (rem*rem*rem);
+        n = n / 10;        
+    }
+    return sum;
+}
+
+int printAmstrong(int n){
+    printf("%d",cubesum(n));
+}
+
+isAmstrong(int n){
+    if(cubesum(n) == n){
+        printf("the number is a amstrong number");
+    }
+    else{
+        printf("the number is not a amstrong number");
+    }
+}
